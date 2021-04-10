0341769931a1a0fd1d453c5784dca810a6098bec
jvitormichels <jvitormichels@gmail.com>
2021-04-10 17:47:59 -0300

    feat: contact id
    * Search for the greatest id and add 1 to generate a new contact

--- arquivos.c
+++ arquivos.c
@@ -6,7 +6,7 @@ void criarContato(struct contato c1) {
 	FILE *arquivo = fopen("contatos.txt","a");
 	
 	if (arquivo != NULL) {
-		int retorno = fprintf(arquivo, "%s %i %s\n", c1.nome, c1.telefone, c1.email);
+		int retorno = fprintf(arquivo, "%i %s %i %s\n", c1.id, c1.nome, c1.telefone, c1.email);
 		
 		
 		if (retorno == EOF) {
@@ -26,9 +26,32 @@ void listarContatos() {
     struct contato c1;
 
     while(!feof(arquivo)){
-        fscanf(arquivo,"%s %d %s ", &c1.nome, &c1.telefone, &c1.email);
+        fscanf(arquivo,"%i %s %d %s ", &c1.id, &c1.nome, &c1.telefone, &c1.email);
         printf("Nome: %s \nTelefone: %d \nEmail: %s\n\n", c1.nome, c1.telefone, c1.email);
     }
     
     fclose(arquivo);
+}
+
+
+int getID(){
+	FILE *arquivo = fopen("contatos.txt","r");
+    
+    struct contato c1;
+    
+	if (arquivo != NULL){
+		
+		while(!feof(arquivo)){
+			fscanf(arquivo,"%i %s %d %s ",&c1.id, &c1.nome, &c1.telefone, &c1.email);
+		}
+		if(feof(arquivo)){
+			int id = c1.id+1;
+			fclose(arquivo);
+			return id;
+		}
+	}
+	else {
+		fclose(arquivo);
+		return 0;
+	}
 }
